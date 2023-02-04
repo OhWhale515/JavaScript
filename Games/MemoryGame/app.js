96222689482822d4ca798172f55730ef82a33533
OhWhale515 <sterlinggreen515@gmail.com>
2023-02-04 17:26:16 -0500

    added images and cardArray random sort

---
+++ Games/MemoryGame/app.js
@@ -0,0 +1,57 @@
+const cardArray = [
+    {
+        name: 'DevOps-Logo',
+        img: 'images/DevOps.png',
+    },
+    {
+        name: 'Sandshrew',
+        img: 'images/sandshrew.png',
+    },
+    {
+        name: 'Psyduck',
+        img: 'images/Psyduck.png',
+    },
+    {
+        name: 'Welcome',
+        img: 'images/pikachu.png',
+    },
+    {
+        name: 'Gasly',
+        img: 'images/gasly.png',
+    },
+    {
+        name: 'Welcome',
+        img: 'images/gameboy.png',
+    },
+    {
+        name: 'Pokeball',
+        img: 'images/pokeball.png',
+    },
+    {
+        name: 'Sandshrew',
+        img: 'images/sandshrew.png',
+    },
+    {
+        name: 'Psyduck',
+        img: 'images/Psyduck.png',
+    },
+    {
+        name: 'Welcome',
+        img: 'images/pikachu.png',
+    },
+    {
+        name: 'Gasly',
+        img: 'images/gasly.png',
+    },
+    {
+        name: 'Welcome',
+        img: 'images/gameboy.png',
+    },
+    {
+        name: 'Pokeball',
+        img: 'images/pokeball.png',
+    },
+
+]
+
+cardArray.sort(() => 0.5 - Math.random())
