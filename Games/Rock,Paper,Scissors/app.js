550e852d305e2f2cc197a6289e7f7f75bca2f13b
OhWhale515 <sterlinggreen515@gmail.com>
2023-02-04 13:35:23 -0500

    created results logic with if statements

--- Games/Rock,Paper,Scissors/app.js
+++ Games/Rock,Paper,Scissors/app.js
@@ -22,4 +22,29 @@ function generateComputerChoice() {
     if (randomNumber === 1) {
         computerChoice = 'scissors'
     }
+    computerChoiceDisplay.innerHTML = computerChoice
+}
+
+function getResult() {
+    if (computerChoice === userChoice) {
+        result = 'its a draw!'
+    }
+    if (computerChoice === 'rock' && userChoice === 'paper') {
+        result = 'you win!'
+    }
+    if (computerChoice === 'rock' && userChoice === 'scissors') {
+        result = 'you lose!'
+    }
+    if (computerChoice === 'paper' && userChoice === 'scissors') {
+        result = 'you win!'
+    }
+    if (computerChoice === 'paper' && userChoice === 'rock') {
+        result = 'you lose!'
+    }
+    if (computerChoice === 'scissors' && userChoice === 'rock') {
+        result = 'you win!'
+    }
+    if (computerChoice === 'scissors' && userChoice === 'paper') {
+        result = 'you lose!'
+    }
 }
