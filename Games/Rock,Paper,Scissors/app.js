38ee71a7097736ce9f4577e35deb5b67a947891a
OhWhale515 <sterlinggreen515@gmail.com>
2023-02-04 13:20:52 -0500

    created logic function if states for cpu choices

--- Games/Rock,Paper,Scissors/app.js
+++ Games/Rock,Paper,Scissors/app.js
@@ -7,4 +7,19 @@ let userChoice
 possibleChoices.forEach(possibleChoices => possibleChoice.addEventListener('click', (e) => {
     userChoice = e.target.id
     userChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice()
 }))
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3) + 1 // or you can use possibleChoices.length
+    
+    if (randomNumber === 1) {
+        computerChoice = 'rock'
+    }
+    if (randomNumber === 1) {
+        computerChoice = 'paper'
+    }
+    if (randomNumber === 1) {
+        computerChoice = 'scissors'
+    }
+}
