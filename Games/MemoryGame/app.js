1d5119cf26fc48628ee542088be4bc5bd59f305d
OhWhale515 <sterlinggreen515@gmail.com>
2023-02-04 17:47:34 -0500

    board logic and flip functions

--- Games/MemoryGame/app.js
+++ Games/MemoryGame/app.js
@@ -51,7 +51,23 @@ const cardArray = [
         name: 'Pokeball',
         img: 'images/pokeball.png',
     },
-
 ]
 
 cardArray.sort(() => 0.5 - Math.random())
+
+const gridDisplay = document.querySelector('#grid')
+
+function createboard () {
+    for (let i = 0; i < 10; i++) {
+        const card = document.createElement('img')
+        card.setAttribute('src', 'images/DevOps.png')
+        card.setAttribute('data-id', i)
+        card.addEventListener('click', flipCard)
+        gridDisplay.appendChild(card)
+    }
+}
+createboard() 
+
+function flipCard() {
+    console.log('click')
+}
